d5afcc14dc4000aa3f5b7650cc01610b69cf0a3f
Isbbrn <30944679+Isbbrn@users.noreply.github.com>
2023-04-27 22:14:39 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,32 +1,12 @@
 import grpc
+import virtualassistant_pb2
+import virtualassistant_pb2_grpc
 
-import chat_pb2
-import chat_pb2_grpc
+channel = grpc.insecure_channel('localhost:50051')
+stub = virtualassistant_pb2_grpc.VirtualAssistantStub(channel)
 
-def send_message(stub, sender, text):
-    message = chat_pb2.Message(sender=sender, text=text)
-    stub.SendMessage(message)
-    print("Message sent successfully.")
-
-def receive_messages(stub):
-    messages = stub.ReceiveMessages(chat_pb2.Empty())
-    for message in messages:
-        print(f"{message.sender}: {message.text}")
-
-def run():
-    channel = grpc.insecure_channel('localhost:50051')
-    stub = chat_pb2_grpc.ChatStub(channel)
-
-    sender = input("Enter your name: ")
-    print("Type '/quit' to exit.")
-
-    while True:
-        text = input("> ")
-        if text == "/quit":
-            break
-        send_message(stub, sender, text)
-
-    channel.close()
-
-if __name__ == '__main__':
-    run()
+while True:
+    message_text = input("Digite a sua mensagem: ")
+    message = virtualassistant_pb2.VirtualAssistantRequest(message=message_text)
+    response = stub.SendMessage(message)
+    print(response.message)
